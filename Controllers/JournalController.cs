7d8ad86b14042bb6f7fa70a5e6ad518158af99b7
Ryan Mathis <ryannoahm@live.com>
2023-10-18 14:23:29 -0500

    started setup of JournalController

---
+++ Controllers/JournalController.cs
@@ -0,0 +1,26 @@
+using CopeAndHope.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CopeAndHope.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JournalController : ControllerBase
+{
+    private CopeAndHopeDbContext _dbContext;
+    public JournalController(CopeAndHopeDbContext context)
+    {
+        _dbContext = context;
+    }
+    [HttpGet("{userId}")]
+    public IActionResult GetCurrentUserJournals(int userId)
+    {
+        return Ok(_dbContext.CopeJournals
+        .Include(j => j.CopeStrategy)
+        .Include(j => j.UserProfile)
+        .Where(j => j.UserProfileId == userId)
+        .OrderBy(j => j.LastUpdated)
+        .ToList());
+    }
+}
